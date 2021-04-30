7213822fcb3b60d55fbebe821e7887bc2d9245b4
Conor <brow4339@umn.edu>
2021-04-30 15:51:53 -0500

    Adding discussion to mainpage.h

--- project/include/mainpage.h
+++ project/include/mainpage.h
@@ -1,5 +1,13 @@
 /*! \mainpage CSCI 3081 Delivery Simulation project
 *
+* \section dev Developer Documentation
+*
+* 1. Navigate to the project/ folder in the repository
+* 2. Compile the project with make
+* 3. Run the project with ./bin/run.sh
+* 4. Go to http://127.0.0.1:8081/
+*
+* 
 * \section team_docs Team Documentation
 *
 * Roles:
@@ -9,7 +17,7 @@
 * Reporter            | Nick \n
 * Project Manager     | Conor \n
 *\n
-* Meetings:
+* \section team_documentation Iteration 3 Team Documentation
 *
 * 4/25/21
 * - Discussed what to implement and 
@@ -18,7 +26,12 @@
 * 4/30/21
 * - Wrapped iter 3 documentation and testing
 *
-* \section Iteration 3 Implementation
+* \section color_change Delivery Agent Color Change
 *
+* For Iteration Three, we decided to implement the color change feature for delivery entities where the color of the entity corresponds to the entity’s battery level. We considered multiple approaches for this and decided to utilize a brute force approach-- if the battery was between 75% and 100% full, for example, the delivery entity would be assigned the color green. To do this, we created a new method in the delivery_agent class named ChangeColor(). This method, which took a pointer to an observer as a parameter, was called at the beginning of the Update() method in order to check the delivery agent’s battery level and change the color accordingly. Within our ChangeColor() method, we check if the battery level has decreased enough to change to a new color; if it has, we update the color and then notify all the observers passed in of the change. This was the most direct approach for our enhancement, but there are several other approaches-- most notably the decorator pattern-- we could have used to implement this functionality as well. If we had used the decorator pattern, the feature could be turned on and off at runtime, making our simulation more customizable. In addition, the decorator pattern would have also helped our program conform to the single responsibility principle; the color change feature would be handled by a separate class rather than within our delivery agent class. With the separation of classes, this would have also closed our delivery agent class to change and created the potential for expansion within our program.
 * 
+* \image html /decorator.jpeg "Example decorator pattern" width = 900 cm
+*
+*
+* \image html /final_uml.png "Final UML" width = 900 cm
 */
